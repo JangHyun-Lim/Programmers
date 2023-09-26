6c646aad605152749f2017dfd75904e9288e03d2
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2023-09-27 02:40:40 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    
+    while (n>0)
+    {
+        if (n%2 == 0) 
+        {
+            answer += n;
+        }
+        n = n-1;
+    }
+    return answer;
+}
