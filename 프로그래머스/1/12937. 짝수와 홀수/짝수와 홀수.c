283f244c6fae735f5d82d68731ad51044da4e08d
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2023-10-20 13:21:06 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+  
+    char* answer = (char*)malloc(sizeof(int));
+    
+    if (num % 2 == 0) {
+        answer = "Even";
+    }
+    else
+        answer = "Odd";
+    
+    return answer;
+
+}
