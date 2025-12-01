0e35d0771473eb666eddf62d8b53036da468015c
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-01 22:57:56 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
-    double res = (double)num1/num2;
-    int answer = 1000*res;
-    return answer;
-}
