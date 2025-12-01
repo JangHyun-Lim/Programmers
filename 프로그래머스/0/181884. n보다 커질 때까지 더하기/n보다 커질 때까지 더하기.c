0cc2746e9290550e6ee0091085063acf23131f3e
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-01 23:31:00 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int numbers[], size_t numbers_len, int n) {
+    int answer = 0;
+    
+    for (int i=0; i<=numbers_len; i++)
+    {
+        if (answer <= n)
+            answer = answer + numbers[i];
+        else
+            break;
+    }
+    return answer;
+}
