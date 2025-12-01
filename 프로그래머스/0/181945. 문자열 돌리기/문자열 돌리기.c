ff3f5647e3579da69efea802540be40251fc6eea
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-01 22:48:14 +0900

    [level 0] Title: 문자열 돌리기, Time: 2.65 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define LEN_INPUT 11
+
+int temp;
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    
+    temp = strlen(s1);
+    
+    for (int i=0; i<temp; i++)
+    {
+        printf("%c\n", s1[i]);
+    }
+
+    return 0;
+}
