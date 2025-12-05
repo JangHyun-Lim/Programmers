5bc4619155c1205e430491db6e0bcfc61a33bf7b
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-05 15:51:12 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int array[], size_t array_len, int n) {
+    int answer = 0;
+    
+    for (int i=0; i < array_len; i++)
+    {
+        if(array[i] - n == 0)
+            answer += 1;
+    }
+    
+    return answer;
+}
