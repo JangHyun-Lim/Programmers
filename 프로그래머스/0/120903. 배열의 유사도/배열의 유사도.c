13974916f0427a37e660d124ef8586cef1325cfd
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-06 20:29:20 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* s1[], size_t s1_len, const char* s2[], size_t s2_len) {
+    int answer = 0;
+    
+    for (int i=0; i<s1_len; i++)
+    {
+        for (int j=0; j<s2_len; j++)
+        {
+            if (strcmp(s1[i], s2[j]) == 0)
+                answer += 1;
+        }
+    }
+    
+    return answer;
+}
