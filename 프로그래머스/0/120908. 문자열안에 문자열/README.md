a56f6e86c1482093c8d8090d9175e879ad962024
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-10 11:55:59 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/README.md
+++ 프로그래머스/0/120908. 문자열안에 문자열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열안에 문자열 - 120908 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120908?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120908) 
 
 ### 성능 요약
 
-메모리: 3.64 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 09일 17:17:05
+2025년 12월 10일 11:55:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -1,28 +1,28 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(string str1, string str2) {
-    int len_str1, len_str2, i, j;
-    len_str1 = 0;
-    len_str2 = 0;
+    int answer = 0;
+    int i, j;
+    int str1_len = str1.size();
+    int str2_len = str2.size();
     
-    while (str1[len_str1] != '\0')
-        len_str1++;
-    
-    while (str2[len_str2] != '\0')
-        len_str2++;
-    
-    for (i = 0; i <= len_str1 - len_str2; i++)
+    for (i = 0; i <= str1_len - str2_len; i++)
     {
-        for (j = 0; j < len_str2; j++)
+        for (j = 0; j < str2_len; j++)
         {
             if (str1[i+j] != str2[j])
                 break;
         }
-        if (j == len_str2)
-            return 1;
+        if (j == str2_len)
+            answer = 1;
     }
-    return 2;
+    
+    if (answer != 1)
+        answer = 2;
+    
+    return answer;
 }
