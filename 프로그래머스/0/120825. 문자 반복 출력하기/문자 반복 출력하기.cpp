3380776c04dc40a4089f1af99091233d2ef643ca
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-12 16:20:55 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    
+    // apple
+    for (int i = 0; i < my_string.size(); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            answer = answer + my_string[i];
+        }
+    }
+    
+    return answer;
+}
