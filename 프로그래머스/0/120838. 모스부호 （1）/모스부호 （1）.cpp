486ab6d1f432b439d703fbf06070248cbbe363da
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-17 14:16:21 +0900

    [level 0] Title: 모스부호 (1), Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -1,42 +1,26 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <iostream>
+#include <sstream>
 
 using namespace std;
+
+string morse[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
 string solution(string letter) {
     string answer = "";
     string temp = "";
-    string arr[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    stringstream ss;
     
-    for (int i = 0; i < letter.size(); i++)
+    ss << letter;
+    while (ss >> temp)
     {
-        if (letter[i] != ' ')
+        for (int i = 0; i < 26; i++)
         {
-            // 공백이 아닐 때까지, 모스부호 연결
-            temp = temp + letter[i];
-        }
-        else // 공백을 만나면 = 문자로 변환
-        {
-            for (int j = 0; j < 26; j++)
+            if (temp == morse[i])
             {
-                if (temp == arr[j])
-                {
-                    answer = answer + char('a' + j);
-                    break;
-                }
+                answer = answer + char('a' + i);
+                break;
             }
-            temp = "";
-        }
-    }
-    
-    // 마지막 문자열 처리
-    for (int j = 0; j < 26; j++)
-    {
-        if (temp == arr[j])
-        {
-            answer = answer + char('a' + j);
         }
     }
     
