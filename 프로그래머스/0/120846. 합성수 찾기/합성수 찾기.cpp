71e97a69d4af89bf371df713a2a89b0a4c95af6a
JangHyun-Lim <145470972+JangHyun-Lim@users.noreply.github.com>
2025-12-23 10:02:37 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 4; i <= n; i++)
+    {   
+        int count = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+                count++;
+            if(count >= 3)
+                break;
+        }
+        
+        if (count >= 3)
+            answer++;
+    }
+    
+    return answer;
+}
